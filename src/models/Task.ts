fe818b43796033c74797dbe30a4561ce5fadbc9a
mura012 <murakoduai@gmail.com>
2023-02-10 11:23:53 +0900

    スキーマを新しく定義

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,23 +1,38 @@
 import mongoose from "mongoose";
 const schema = new mongoose.Schema({
-  userId: { type: String },
-  task: {
-    type: String,
-    trimu: true,
-    maxlength: [20, "タスク名は20文字以内で入力してください"],
-  },
-  limit: {
-    type: String,
-    enum: ["今日中", "今週中", "今月中", "継続"],
-  },
-  importance: {
-    type: String,
-    enum: ["高", "中", "低"],
-  },
-  isDone: {
-    type: Boolean,
-    default: false,
-  },
+  name: { type: String, required: true },
+  adminUserName: { type: String },
+  adminUserEmail: { type: String },
+  adminUserPhoto: { type: String },
+  tasks: [
+    {
+      task: {
+        type: String,
+        trimu: true,
+        maxlength: [20, "タスク名は20文字以内で入力してください"],
+      },
+      limit: {
+        type: String,
+        enum: ["今日中", "今週中", "今月中", "継続"],
+      },
+      importance: {
+        type: String,
+        enum: ["高", "中", "低"],
+      },
+      isDone: {
+        type: Boolean,
+      },
+      workingUserName: {
+        type: String,
+      },
+    },
+  ],
+  teamUser: [
+    {
+      name: { type: String },
+      email: { type: String },
+    },
+  ],
 });
 
 const TaskSchema = mongoose.model("Task", schema);
